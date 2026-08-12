23d246c07fe0e6e2f33b760916f87bafe59c20ff
Kush Singh Parihar <kushsinghparihar005@gmail.com>
2026-08-12 17:19:23 +0530

    Add frontend home route

--- Backend/server.js
+++ Backend/server.js
@@ -269,6 +269,12 @@ catch (error) {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.get("/", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "../Frontend/login.html")
+    );
+});
+
+app.listen(PORT, () => {
     console.log(`Server Running On Port ${PORT}`);
 });
